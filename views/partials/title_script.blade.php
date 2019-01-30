e6b89a4f3da90fb579f1850981a26f3dbb44e5f5
Wedding DJ <chris@ideaspacevr.org>
2019-01-30 12:37:31 -0500

    workaround for not showing nav arrow textures: nav arrows need html shader material refresh

--- views/partials/title_script.blade.php
+++ views/partials/title_script.blade.php
@@ -6,3 +6,16 @@ for (var i = 0; i < post_title_textures.length; i++) {
 				document.querySelector('#post-title-' + post_title_textures[i].dataset.cid).components.material.shader.__render();
 		}
 }
+
+var nav_arrow_up_all = document.querySelectorAll('.navigation-arrow-up');
+for (var i = 0; i < nav_arrow_up_all.length; i++) {
+		/* workaround to refresh texture, otherwise it is not appearing on first loading of page */
+		nav_arrow_up_all[i].components.material.shader.__render();
+}
+
+var nav_arrow_down_all = document.querySelectorAll('.navigation-arrow-down');
+for (var i = 0; i < nav_arrow_down_all.length; i++) {
+		/* workaround to refresh texture, otherwise it is not appearing on first loading of page */
+		nav_arrow_down_all[i].components.material.shader.__render();
+}
+
