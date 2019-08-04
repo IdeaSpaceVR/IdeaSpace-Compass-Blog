c619c1c9b514585b1c17437e95f68e74bae98438
Wedding DJ <chris@ideaspacevr.org>
2019-08-04 02:47:35 +0000

    removed rotation

--- views/partials/post_title.blade.php
+++ views/partials/post_title.blade.php
@@ -13,7 +13,7 @@
 				isvr-blog-post-nav-up="id: navigation-arrow-up-{{ $blog_post['post-title-north']['#content-id'] }}; cid: {{ $blog_post['post-title-north']['#content-id'] }}"
 				geometry="primitive: plane; width: 1; height: 1"
 				position="-1.15 0 -0.001" 
-				rotation="0 20 0" 
+				rotation="0 0 0" 
 				material="shader: html; side: double; target: #navigation-arrow-up-texture; transparent: true; ratio: width">
 		</a-entity>
 		@else
@@ -22,7 +22,7 @@
 				class="navigation-arrow-up"
 				geometry="primitive: plane; width: 1; height: 1"
 				position="-1.15 0 -0.001" 
-				rotation="0 20 0" 
+				rotation="0 0 0" 
 				material="shader: html; side: double; target: #navigation-arrow-up-inactive-texture; transparent: true; ratio: width">
 		</a-entity>
 		@endif
@@ -33,7 +33,7 @@
 				isvr-blog-post-nav-down="id: navigation-arrow-down-{{ $blog_post['post-title-north']['#content-id'] }}; cid: {{ $blog_post['post-title-north']['#content-id'] }}; next_page_url: {{ $space_url }}/content/blog-posts?per-page={{ $max_posts }}&page=2; meters: {{ $meters_between_posts }}; posts_per_page: {{ $max_posts }}; total_posts: {{ count($content['blog-posts']) }}; post_counter: {{ $post_counter }}"
 				geometry="primitive: plane; width: 1; height: 1"
 				position="1.15 0 -0.001" 
-				rotation="0 -20 0" 
+				rotation="0 0 0" 
 				material="shader: html; side: double; target: #navigation-arrow-down-texture; transparent: true; ratio: width">
 		</a-entity>
 		@else
@@ -42,7 +42,7 @@
 				class="navigation-arrow-down"
 				geometry="primitive: plane; width: 1; height: 1"
 				position="1.15 0 -0.001" 
-				rotation="0 -20 0" 
+				rotation="0 0 0" 
 				material="shader: html; side: double; target: #navigation-arrow-down-inactive-texture; transparent: true; ratio: width">
 		</a-entity>
 		@endif
